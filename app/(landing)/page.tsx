a729ebd15031abfdcda19df5ff9ba5dfd01daf8e
auziqni <65333301+auziqni@users.noreply.github.com>
2024-07-17 21:13:32 +0700

    feat: landing hero

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,3 +1,15 @@
+import Image from "next/image";
 export default function Home() {
-  return <main className="h-screen w-screen"></main>;
+  return (
+    <main className="">
+      <div id="hero" className="relative h-screen w-screen">
+        <Image
+          src="https://tp.auziqni.com/aafo/heropic.png"
+          alt="hero"
+          layout="fill"
+          objectFit="cover"
+        />
+      </div>
+    </main>
+  );
 }
